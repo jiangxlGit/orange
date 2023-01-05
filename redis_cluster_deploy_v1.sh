5207b4f982c6f35f2dda8fec6510a331539a2112
jiangxl <jiangxl8080@163.com>
2023-01-06 00:54:40 +0800

    完善redis脚本

---
+++ redis_cluster_deploy_v1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+clear;
+
+echo '================================================================';
+echo '完成安装redis集群脚本';
+echo '================================================================';
+
+echo "配置redis集群..."
+/usr/local/bin/redis-cli --cluster create $1:7000 $1:7001 $1:7002 $2:7000 $2:7001 $2:7002 --cluster-replicas 1 -a Jiang13479@
+echo "配置完成!"
+
+# generate redis-cluster service file
+cat << EOT > $BASE_DIR/redis-cluster.service
+[Unit]
+Description=Redis 7.0 Cluster Service
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/local/redis-cluster/startup.sh
+
+[Install]
+WantedBy=default.target
+EOT
+
+# create service
+echo "创建redis集群服务..."
+ln -s $BASE_DIR/$SERVICE /etc/systemd/system/$SERVICE 
+sudo systemctl daemon-reload && sudo systemctl enable $SERVICE && sudo systemctl start $SERVICE
+
+# Cluster OK
+echo ""
+echo "完成集群创建!"
+echo ""
+echo "测试集群命令: /usr/local/bin/redis-cli -h 127.0.0.1 -p 7000"
+echo ""
+echo "127.0.0.1:7000>auth Jiang13479@"
+echo ""
+echo "127.0.0.1:7000>cluster nodes"
+
+echo '================================================================';
+echo '完成安装redis集群脚本';
+echo '================================================================';
---
+++ redis_cluster_uninstall_v1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear;
+
+echo '================================================================';
+echo '开始卸载redis服务';
+echo '================================================================';
+
+# 杀死redis服务器
+ps -ef | grep redis-server | grep cluster | awk '{print $2}' | xargs kill -9 
+
+# 禁用systemd
+systemctl disable redis-cluster.service
+
+# 删除redis集群目录
+if [ -f $BASE_DIR ]; then
+rm -rf $BASE_DIR
+fi
+
+echo '================================================================';
+echo '完成卸载redis服务';
+echo '================================================================';
