a7dbd5eb06660fc74118134acb7e79f8f5498022
jiangxlGit <jiangxl13479@163.com>
2023-02-01 12:08:20 +0800

    update nacos

--- nacos_cluster_install_v1.sh
+++ nacos_cluster_install_v1.sh
@@ -9,7 +9,7 @@ nacosVer="nacos-server-$verNo"
 # mysql密码
 mysqlPWD="Jiang13479@"
 # mysql端口
-mysqlPort="23307"
+mysqlPort="3306"
 # 存放目录
 storageDir=/data/soft
 # 安装目录
@@ -28,7 +28,7 @@ yum -y install wget
 cd $storageDir
 if [ ! -f $storageDir/$nacosVer* ];then
 	echo "-------下载nacos安装包-------"
-	wget -i -c    https://github.com/alibaba/nacos/releases/download/$verNo/$nacosVer.tar.gz
+	wget -i -c https://github.com/alibaba/nacos/releases/download/$verNo/$nacosVer.tar.gz
 else
     echo "-------nacos安装包已存在-------"
 fi
@@ -44,7 +44,7 @@ fi
 
 echo "-------登录mysql并执行mysql-schema.sql-------"
 cd $installDir/nacos/conf
-mysql -uroot -p"$mysqlPWD" << EOF
+mysql -h 43.139.242.81 -P"$mysqlPort" -uroot -p"$mysqlPWD" << EOF
     CREATE DATABASE IF NOT EXISTS nacos_config;
     use nacos_config;
     source mysql-schema.sql;
@@ -55,13 +55,22 @@ echo "-------修改application.properties-------"
 echo "##数据源使用mysql" > $installDir/nacos/conf/application.properties
 echo "spring.datasource.platform=mysql" >> $installDir/nacos/conf/application.properties
 echo "### Count of DB:" >> $installDir/nacos/conf/application.properties
-echo "db.num=1" >> $installDir/nacos/conf/application.properties
+echo "db.num=2" >> $installDir/nacos/conf/application.properties
 echo "### Connect URL of DB:" >> $installDir/nacos/conf/application.properties
-echo "db.url.0=jdbc:mysql://127.0.0.1:$mysqlPort/nacos_config?characterEncoding=utf8&connectTimeout=1000&socketTimeout=3000&autoReconnect=true&useUnicode=true&useSSL=false&serverTimezone=UTC" >> $installDir/nacos/conf/application.properties
-echo "db.user.0=root" >> $installDir/nacos/conf/application.properties
-echo "db.password.0=$mysqlPWD" >> $installDir/nacos/conf/application.properties
+echo "db.url.0=jdbc:mysql://43.139.242.81:$mysqlPort/nacos_config?characterEncoding=utf8&connectTimeout=1000&socketTimeout=3000&autoReconnect=true&useUnicode=true&useSSL=false&serverTimezone=UTC" >> $installDir/nacos/conf/application.properties
+echo "db.url.1=jdbc:mysql://43.139.96.249:$mysqlPort/nacos_config?characterEncoding=utf8&connectTimeout=1000&socketTimeout=3000&autoReconnect=true&useUnicode=true&useSSL=false&serverTimezone=UTC" >> $installDir/nacos/conf/application.properties
+echo "db.user=root" >> $installDir/nacos/conf/application.properties
+echo "db.password=$mysqlPWD" >> $installDir/nacos/conf/application.properties
+
+echo "-------修改cluster.conf-------"
+cp $installDir/nacos/conf/cluster.conf.example $installDir/nacos/conf/cluster.conf
+echo "##配置集群" >> $installDir/nacos/conf/cluster.conf
+echo "106.14.17.131:8848" >> $installDir/nacos/conf/cluster.conf
+echo "43.139.242.81:8848" >> $installDir/nacos/conf/cluster.conf
+echo "43.139.96.249:8848" >> $installDir/nacos/conf/cluster.conf
+
 
 echo "-------启动nacos-------"
 cd $installDir/nacos/bin
-sh  startup.sh -m standalone &
+sh  startup.sh
 echo "@@@@@ nacos安装完成 @@@@@"
